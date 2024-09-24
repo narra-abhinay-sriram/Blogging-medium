b379da98ce8059c69477e32eb57ce04950140313
Abhinay <121sriramreddy@gmail.com>
2024-09-24 18:29:50 +0530

    deployed backend,login page done

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,20 +1,55 @@
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 const Signup = () => {
-    
+    const [email,setmail]=useState("")
+    const [password,setpass]=useState("")
+    const [name,setname]=useState("")
+    const[error,seterror]=useState("")
+    const navigate=useNavigate()
+
+
+
   return (
     <div>
      <form onSubmit={(e)=>{e.preventDefault()}}>
        <p className="text-xs mt-1 font-semibold">Email</p>
-       <input className="w-72 border border-gray-400 mt-1 px-2 py-1 rounded-md"type="text" placeholder="john@gmail.com"/>
+       <input onChange={(e)=>{setmail(e.target.value)}}
+       className="w-72 border border-gray-400 mt-1 px-2 py-1 rounded-md"type="text" placeholder="john@gmail.com"/>
        <p className="text-xs mt-3 font-semibold">Full Name</p>
-       <input className="w-72 border border-gray-400 mt-1 px-2 py-1 rounded-md"type="text" placeholder="John Wick"/>
+       <input onChange={(e)=>{setname(e.target.value)}}
+       className="w-72 border border-gray-400 mt-1 px-2 py-1 rounded-md"type="text" placeholder="John Wick"/>
        <p className="text-xs mt-3 font-semibold">Password</p>
-       <input className="w-72 border border-gray-400 mt-1 px-2 py-1 rounded-md" type="password" placeholder="1234@1234"/>
+       <input onChange={(e)=>{setpass(e.target.value)}}
+       className="w-72 border border-gray-400 mt-1 px-2 py-1 rounded-md" type="password" placeholder="1234@1234"/>
       <div>
-       <button className="w-72 border bg-black border-gray-300 mt-4 hover:bg-opacity-55 px-2 py-1 rounded-md text-white">SignUp</button>
+       <button onClick={async()=>{
+        try  {
+                const resp=await axios.post("https://backend.121sriramreddy.workers.dev/api/v1/user/signup",{email,password,name})
+                const token=resp.data.jwt
+                console.log(resp)
+                if(resp.data.jwt){
+                    localStorage.setItem("token",token)
+                    navigate("/blogs")
+                    console.log("hi")
+            
+                 }else seterror(resp.data.message)
+                }
+                 catch (error) {
+                    if (typeof error === "object" && error !== null && "response" in error) {
+                      const err = error as { response: { data: { message: string } } };
+                      seterror(err.response.data.message);
+                    } else {
+                      seterror("An unexpected error occurred. Please try again.");
+                    }
+                    
+                  }
+       }} 
+       className="w-72 border bg-black border-gray-300 mt-4 hover:bg-opacity-55 px-2 py-1 rounded-md text-white">SignUp</button>
        </div>
-
-
+     {error && <p className=" text-gray-400 ml-5">{error}</p>
+}
      </form>
     </div>
   )
